b5056f6b2cfcd6f6eefdf4dd772a193628ad3464
Razzaque1991 <140472349+Razzaque1991@users.noreply.github.com>
2024-04-30 08:55:26 +0600

    Add files via upload

---
+++ click-change-backkground/script.js
@@ -0,0 +1,11 @@
+
+// script.js
+document.getElementById('darkIcon').addEventListener('click', () => {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+});
+
+document.getElementById('lightIcon').addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+});
